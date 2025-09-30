12c751dd8b401cd62e4cc845d0dd2f48aebd1313
Fedor Evstigneev <feda12348@mail.ru>
2025-09-30 14:55:19 +0300

    Лабораторная работа №2. Вариант 5. Задание 8

--- Laboratory_Work_2/Laboratory_Work_2_Variant_5.py
+++ Laboratory_Work_2/Laboratory_Work_2_Variant_5.py
@@ -12,11 +12,50 @@ def list_prime_numbers():
 for x in list_prime_numbers():
     print(x, end = " ")
 print()
-# Подсчёт гласных
-list_vowels = ['а', 'о', 'у', 'э', 'ы', 'я', 'ю', 'е', 'ё', 'и']
-counter = 0
-string = str(input())
-for i in range (len (string)):
-    if string[i] in list_vowels:
-        counter += 1
-print(counter)
+
+# # Подсчёт гласных
+# list_vowels = ['а', 'о', 'у', 'э', 'ы', 'я', 'ю', 'е', 'ё', 'и']
+# counter = 0
+# string = str(input())
+# for i in range (len (string)):
+#     if string[i] in list_vowels:
+#         counter += 1
+# print(counter)
+
+# Таблица умножения
+def multiplication_table(size):
+    print("   |", end = "")
+    for i in range(1, size + 1):
+        print(f"{i:4}",end = "")
+    print("\n"
+          + "---+"
+          + size * "----")
+    for i in range(1, size + 1):
+        print(f"{i:2} |", end = "")
+        for j in range(1, size + 1):
+            print(f"{i*j:4}", end = "")
+        print()
+multiplication_table(20)
+
+# Бинарный поиск
+def binary_search(arr, x):
+    left = 0
+    right = len(arr) - 1
+    mid = 0
+    while left <= right:
+        mid = (right + left) // 2
+        if arr[mid] < x:
+            left = mid + 1
+        elif arr[mid] > x:
+            right = mid - 1
+        else:
+            return mid
+    return -1
+
+arr = [ 0, 1, 3, 3, 12 ]
+x = 13
+result = binary_search(arr, x)
+if result != -1:
+    print(f"Элемент {x} найден с индексом:", str(result))
+else:
+    print(f"Элемент {x} не найден")
