ce4ef18546b2074ed132850341cf5209d03097cf
Evstigneev Fedor <feda12348@mail.ru>
2025-11-06 12:19:22 +0300

    Добавлен класс Book с полями Книга, Автор

---
+++ Laboratory_Work_3/Variant_5.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def __str__(self):
+        return f"Книга: {self.title}, Автор: {self.author}"
