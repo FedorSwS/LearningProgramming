2820672bca5d3a6e1af37cf048e04ee801ed81a0
Evstigneev Fedor <feda12348@mail.ru>
2025-11-06 12:28:21 +0300

    Добавлено наследование класса Dog от Animal

--- Laboratory_Work_3/Variant_5.py
+++ Laboratory_Work_3/Variant_5.py
@@ -4,3 +4,12 @@ class Book:
         self.author = author
     def __str__(self):
         return f"Книга: {self.title}, Автор: {self.author}"
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        pass
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} говорит Гав!"
