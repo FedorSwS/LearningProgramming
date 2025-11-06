8c9deea20db94d4a08588eba6f352bff3d3b0659
Evstigneev Fedor <feda12348@mail.ru>
2025-11-06 12:30:05 +0300

    Добавлен класс Vector с перегрузкой оператора +

--- Laboratory_Work_3/Variant_5.py
+++ Laboratory_Work_3/Variant_5.py
@@ -13,3 +13,12 @@ class Animal:
 class Dog(Animal):
     def speak(self):
         return f"{self.name} говорит Гав!"
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+    def __str__(self):
+        return f"Vector({self.x},{self.y})"
