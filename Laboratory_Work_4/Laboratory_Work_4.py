b47e89219b1e754a334390dc05c64aa6fcfb4493
Evstigneev Fedor <feda12348@mail.ru>
2025-11-17 03:08:31 +0300

    Добавлен генератор Фибоначчи

--- Laboratory_Work_4/Laboratory_Work_4.py
+++ Laboratory_Work_4/Laboratory_Work_4.py
@@ -1,3 +1,11 @@
 def squares_of_numbers():
     return list(map(lambda x: x ** 2, range(1, 11)))
 print("Квадраты чисел 1–10:", squares_of_numbers())
+
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+fib = fibonacci_generator()
+print([next(fib) for i in range(20)])
