e679c14623b71f30ec3273dec654dbbf0eba4518
Evstigneev Fedor <feda12348@mail.ru>
2025-11-17 04:04:34 +0300

    Добавлен декоратор с параметром округления чисел

--- Laboratory_Work_4/Laboratory_Work_4.py
+++ Laboratory_Work_4/Laboratory_Work_4.py
@@ -20,3 +20,15 @@ def log_calls(func):
 def squares(numbers):
     return list(map(lambda x: x ** 2, numbers))
 squares([1,2,4,6,8,10])
+
+def round_list_result(digits = 2):
+    def decorator(func):
+        def wrapper(*args):
+            result = func(*args)
+            return [f"{x:.{digits}f}" for x in result]
+        return wrapper
+    return decorator
+@round_list_result(digits = 3)
+def list_for_round():
+    return [3.159, 2.7128, 1.41, 0.577212]
+print(list_for_round())
