21ef1fb1f83627e7d501e598993e11ec80a8b705
Evstigneev Fedor <feda12348@mail.ru>
2025-11-25 10:43:40 +0300

    Добавлено меню File->Exit

--- Laboratory_Work_5/Variant_5_57.py
+++ Laboratory_Work_5/Variant_5_57.py
@@ -12,12 +12,22 @@ def calculate_sum():
         label_sum_result.config(text=f"{result}")
     except ValueError:
         messagebox.showerror("Ошибка", "Введите числа")
-        
+ 
+def exit_app():
+    if messagebox.askokcancel("Выход", "Вы действительно хотите выйти?"):
+        root.destroy()
+
 root = tk.Tk()
 root.title("ЛР №5")
 root.geometry("400x200")
 notebook = ttk.Notebook(root)
 notebook.pack(fill="both", expand=True)
+
+menubar = tk.Menu(root)
+file_menu = tk.Menu(menubar, tearoff=0)
+file_menu.add_command(label="Exit", command=exit_app)
+menubar.add_cascade(label="File", menu=file_menu)
+root.config(menu=menubar)
 
 tab_hello = ttk.Frame(notebook)
 notebook.add(tab_hello, text="Приветствие")
